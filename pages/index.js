f1d18b4f8656deb8ddbe73ca9fc42237a0318acc
Damian Lis <foxprogramming93@gmail.com>
2021-03-08 23:46:33 +0100

    Created basic layout which wraps about.js, index.js, projects.js pages

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
+import Layout from 'components/Layout';
 import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Blog - recent posts </title>
       </Head>
-
-      <main>
-        <h1 className="text-center">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
+      <p>Tutaj będzie treść</p>
+    </Layout>
   );
 }
