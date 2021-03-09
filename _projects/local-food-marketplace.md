29ec98a6ad7d5657a05a823782f24054e0392a61
Damian Lis <foxprogramming93@gmail.com>
2021-03-09 11:33:06 +0100

    Installed grey-matter, created parser for project files  and created fn in services for sort data

--- _projects/local-food-marketplace.md
+++ _projects/local-food-marketplace.md
@@ -1,7 +1,7 @@
 ---
 title: Local food marketplace
 description: Web application for homemade food sellers. Filtering, sorting and searching offers. Paid advertisments system.
-date: 12-10-2011
+date: 12-10-1999
 tags:
   - PHP
   - HTML5
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
 import Layout from 'components/Layout';
 import Head from 'next/head';
-import { getList } from 'lib/markdownParser';
 
-export const getStaticProps = () => {
-  const projects = getList('_projects');
-
-  return {
-    props: { projects }
-  };
-};
-
-export default function Home({ projects }) {
-  console.log(projects);
+export default function Home() {
   return (
     <Layout>
       <Head>
