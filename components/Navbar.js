1e323ce661496bbd257bf44b251e37465fe98b3e
Damian Lis <foxprogramming93@gmail.com>
2021-03-10 15:03:47 +0100

    Modified mobile navigation to display right links

--- components/Navbar.js
+++ components/Navbar.js
@@ -72,26 +72,16 @@ export default function Navbar() {
       {mobileMenu && (
         <div className="sm:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1">
-            <a
-              href="#"
-              className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className=" text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Team
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Projects
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Calendar
-            </a>
+            {navLinks.map((link) => (
+              <Link href={link.path} key={link.id}>
+                <a
+                  className={`${
+                    router.pathname === link.path ? ' bg-gray-900' : ''
+                  }  text-white block px-3 py-2 rounded-md text-base font-medium`}>
+                  {link.label}
+                </a>
+              </Link>
+            ))}
           </div>
         </div>
       )}
