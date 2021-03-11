f4a0125618eec4062c4ef4720cffe432e496cde2
Damian Lis <foxprogramming93@gmail.com>
2021-03-11 10:17:33 +0100

    Created Loader.js with styles and implemented it in Layout.js

--- containers/Layout.js
+++ containers/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
+import Loader from 'components/Loader';
 
 export default ({ children }) => {
   return (
     <>
+      <Loader />
       <Navbar />
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
         <main className="min-h-screen">{children}</main>
