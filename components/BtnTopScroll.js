22ee290599b3b3793457ff07ef28834acfbb8c46
Damian Lis <foxprogramming93@gmail.com>
2021-03-12 18:59:55 +0100

    Modified btnTopScroll.js added new styles relating to the appearance of the button

--- components/BtnTopScroll.js
+++ components/BtnTopScroll.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react"
 
 export default () => {
-        const [show, setShow] = useState(false)
+        const [isShow, setIsShow] = useState(false)
         const [isFooter, setIsFooter] = useState(false)
    
     const handleScrollToTop = () =>{
@@ -17,10 +17,10 @@ export default () => {
     }
 
     const handleShowScrollBtn = () =>{
-        if (window.scrollY > 100 ) {
-            setShow(true);  
+        if (window.scrollY > 200 ) {
+            setIsShow(true);  
           } else {
-            setShow(false)     
+            setIsShow(false)     
           }
     }
    
@@ -31,13 +31,16 @@ export default () => {
         })
     }, [])
 
+    console.log(isShow)
+
     return(
         
-           show && <div className={` transition-all bg-red-200 mx-auto fixed ${isFooter? "bottom-20" : "bottom-5"} w-full`}>
-                <div className="relative bg-green-200 max-w-5xl mx-auto">
-        <div onClick={handleScrollToTop} className="bg-yellow-500 p-7 absolute right-5 bottom-0 h-3 w-3 rounded-full flex justify-center items-center  ml-auto text-2xl text-blue-900 bg-opacity-90 cursor-pointer">
+            // <div className={` ${isShow ? (isFooter? "bottom-20": "bottom-5") : "bottom-50" }transition-all bg-red-200 mx-auto fixed  w-full`}>
+            <div className={` ${isShow ? (isFooter? "bottom-20": "bottom-10") : "-bottom-20" } transition-all  mx-auto fixed  w-full`}>
+                <div className="relative  max-w-5xl mx-auto">
+        <button onClick={handleScrollToTop} className="focus:outline-none bg-yellow-500 p-7 absolute right-5 bottom-0 h-3 w-3 rounded-full flex justify-center items-center  ml-auto text-2xl text-blue-900 bg-opacity-90 cursor-pointer">
             <i className="fas fa-arrow-up "></i>
-            </div>
+            </button>
         </div>
         </div>
     )
