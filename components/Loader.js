b94eef7a7de78824d5bb536fec69cbebcff177ce
Damian Lis <foxprogramming93@gmail.com>
2021-03-12 13:28:37 +0100

    Added feature to Loader.js that show info when loading is to long

--- components/Loader.js
+++ components/Loader.js
@@ -1,23 +1,36 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import LoadingContext from 'contexts/loading';
 
 export default () => {
   const { load } = useContext(LoadingContext.store);
+  const [showInfo, setShowInfo] = useState(false)
+  let timeoutIndex
+  const time = 2000
+ 
 
   useEffect(() => {
     if (load) {
       document.body.style.overflow = 'hidden';
+      timeoutIndex = setTimeout(()=>{
+        setShowInfo(true)
+      }, time)
+
     } else {
+      setShowInfo(false)
+      clearTimeout(timeoutIndex)
       document.body.style.overflow = 'auto';
     }
-  });
+
+  }, [load]);
 
   return (
     load && (
-      <div className=" flex flex-col justify-center items-center h-screen w-screen bg-blue-900 bg-opacity-60 fixed z-10 overflow-hidden">
+      <div className=" flex flex-col justify-center items-center h-screen w-screen bg-gray-900 bg-opacity-60 fixed z-10 overflow-hidden">
         <div className="object-center loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
         <p className="mt-4 text-lg leading-relaxed text-white">Wczytuję artykuł...</p>
+        {showInfo && <p className="px-5 text-center mt-1 text-lg leading-relaxed text-white">Strona ładuje się dłuzej niz zwyklę... Prosimy o cierpliwość!</p>}
       </div>
+      
     )
   );
 };
