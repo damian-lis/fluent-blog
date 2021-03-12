a6a3fad4deaa8e050a41044523db0b3fd6b0832d
Damian Lis <foxprogramming93@gmail.com>
2021-03-12 18:44:30 +0100

    Added fixed style for navbar and created empty div to fill space

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
   const router = useRouter();
 
   return (
-    <nav className="bg-blue-900">
+    <nav className="fixed w-full bg-blue-900 z-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
--- containers/Layout.js
+++ containers/Layout.js
@@ -6,7 +6,8 @@ export default ({ children }) => {
       <Loader />
       <Navbar />
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
-        <main className="min-h-screen">{children}</main>
+        <div className="h-16 bg-red-200"></div>
+        <main className="min-h-screen ">{children}</main>
       </div>
       <Footer />
       <BtnTopScroll/>
