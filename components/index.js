bbb4c875e79361d816c8716a95a10e3ac67d7f7c
Damian Lis <foxprogramming93@gmail.com>
2021-06-09 13:25:59 +0200

    Renamed Tag component related to 275438d commit

--- components/index.js
+++ components/index.js
@@ -3,7 +3,7 @@ import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
 import Loader from 'components/Loader';
 import ProjectCard from 'components/ProjectCard';
-import Tag from 'components/Tag';
+import ProjectTag from 'components/ProjectTag';
 import BtnTopScroll from 'components/BtnTopScroll';
 
-export { ArticleCard, Footer, Navbar, Loader, ProjectCard, Tag, BtnTopScroll };
+export { ArticleCard, Footer, Navbar, Loader, ProjectCard, ProjectTag, BtnTopScroll };
--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
 import Layout from 'containers/Layout';
 import Head from 'next/head';
 import { getAllProjects } from 'services/projects';
-import { ProjectCard, Tag } from 'components';
+import { ProjectCard, ProjectTag } from 'components';
 
 export const getStaticProps = () => {
   const projects = getAllProjects();
@@ -22,7 +22,7 @@ export default function Projects({ projects }) {
           <h1 className="text-center text-3xl mb-10 mt-10">MY PROJECTS</h1>
           <ul className="mb-8">
             {projects.map((project) => (
-              <ProjectCard key={project.title} project={project} TagComponent={Tag} />
+              <ProjectCard key={project.title} project={project} TagComponent={ProjectTag} />
             ))}
           </ul>
         </div>
